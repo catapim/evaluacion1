1faa10e2df315e7703861bfec58cd29b1f8224be
toxocarrier <catalinapimentel@gmail.com>
2018-12-12 17:19:19 -0300

    custom adapter fail

--- app/src/main/java/evaluacion1/evaluacion1/ClienteAdapter.kt
+++ app/src/main/java/evaluacion1/evaluacion1/ClienteAdapter.kt
@@ -7,21 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import evaluacion1.evaluacion1.R.id.parent
+import evaluacion1.evaluacion1.R.id.rvEj
 
-class ClienteAdapter (var item: ArrayList<Cliente>) : RecyclerView.Adapter<ClienteAdapter.ViewHolder>(){
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getItemCount(): Int {
-        return item.size
-    }
-
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    class ViewHolder(row: View): RecyclerView.ViewHolder(row){
-
-    }
+class ClienteAdapter(){
 }
--- app/src/main/java/evaluacion1/evaluacion1/Registro.kt
+++ app/src/main/java/evaluacion1/evaluacion1/Registro.kt
@@ -1,58 +1,104 @@
 package evaluacion1.evaluacion1
 
+import android.content.Context
 import android.os.Bundle
+import android.support.constraint.R.id.parent
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.support.v7.widget.RecyclerView
-import android.text.Layout
 import android.widget.*
-import evaluacion1.evaluacion1.R.id.*
 import kotlinx.android.synthetic.main.activity_registro.*
+import kotlinx.android.synthetic.main.activity_registro.view.*
+
 class Registro : AppCompatActivity() {
 
 
-    var recyclerView:RecyclerView?=null
+    var recyclerView: RecyclerView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
 
-        recyclerView = findViewById(R.id.rvEj)
         //   var vista: View = LayoutInflater.from(contexto).inflate(recurso, null)
-        var lista_clientes=arrayListOf<Cliente>();
+        var lista_clientes = arrayListOf<Cliente>()
         var buttonGuardar = findViewById<Button>(R.id.btnGuardar)
         var buttonOtro = findViewById<Button>(R.id.btnOtro)
 
         //boton que guarda datos cliente en arraylist
         buttonGuardar.setOnClickListener {
-            var rutTextBox:EditText = findViewById<EditText>(R.id.inRut)
-            System.out.println(rutTextBox.text);
-            var nombre:String = findViewById<EditText>(R.id.inNombre).text.toString();
-            var apellido:String = findViewById<EditText>(R.id.inApellido).text.toString()
-            var rut:String = findViewById<EditText>(R.id.inRut).text.toString()
-            var comuna:String = findViewById<EditText>(R.id.inComuna).text.toString()
-            var direccion:String = findViewById<EditText>(R.id.inDireccion).text.toString()
+            //var rutTextBox:EditText = findViewById<EditText>(R.id.inRut)
+            //System.out.println(rutTextBox.text);
+            var nombre: String = findViewById<EditText>(R.id.inNombre).text.toString()
+            var apellido: String = findViewById<EditText>(R.id.inApellido).text.toString()
+            var rut: String = findViewById<EditText>(R.id.inRut).text.toString()
+            var comuna: String = findViewById<EditText>(R.id.inComuna).text.toString()
+            var direccion: String = findViewById<EditText>(R.id.inDireccion).text.toString()
 
-            var cliente:Cliente = Cliente(nombre,apellido,rut,comuna,direccion)
-            lista_clientes.add(cliente);
-
+            var cliente: Cliente = Cliente(nombre, apellido, rut, comuna, direccion)
+            lista_clientes.add(cliente)
             //Toast.makeText(this, "hola "+cliente.nombre+" "+cliente.apellido, Toast.LENGTH_SHORT).show()
             Toast.makeText(this, "Cliente registrado exitosamente", Toast.LENGTH_SHORT).show()
 
         }
         //botón que limpia los campos edittext para registrar nuevo cliente
-        buttonOtro.setOnClickListener{
+        buttonOtro.setOnClickListener {
             inRut.text.clear()
             inNombre.text.clear()
             inApellido.text.clear()
             inComuna.text.clear()
             inDireccion.text.clear()
+            Toast.makeText(this, "Ya puedes registrar otro cliente", Toast.LENGTH_SHORT).show()
 
         }
 
+        //var miContexto:Context
+
+
+        /*class CustomAdapter (var miContexto:Context,
+                            // var miRecurso:Int,
+                             var miLista:ArrayList<Cliente>)
+                            :ArrayAdapter<Cliente>(miContexto,miRecurso,miLista){
+
+            override fun getView(position: Int,
+                                 convertView: View?,
+                                 parent: ViewGroup?): View {
+                val item : View = LayoutInflater.from(miContexto).inflate(miRecurso, null)
+                val cliente:Cliente = miLista[position]
+               // return super.getView(position,convertView, parent)
+                return item
+            }
+        }*/
 
     }
 
+    /*class CustomAdapter(
+        val miContexto: Context,
+        val miLista: ArrayList<Cliente>
+    ) : RecyclerView.Adapter<CustomAdapter.CustomViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup,viewType:Int): CustomViewHolder {
+            val v:View = LayoutInflater.from(miContexto).inflate(R.layout.activity_listar,parent,false)
+            return CustomViewHolder(v)
+        }
 
+        override fun getItemCount(): Int {
+            return miLista.size
+        }
+
+        override fun onBindViewHolder(holder:CustomViewHolder, position: Int) {
+            holder.bindata(miLista[position])
+        }
+
+        class CustomViewHolder (val miVista:View):RecyclerView.ViewHolder(miVista){
+            fun bindata(cliente: Cliente) {
+                miVista.inNombre.text = cliente.nombre
+            }
+        }
+    }*/
 }
+
+
+
+
